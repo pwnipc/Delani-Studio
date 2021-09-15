48a092629ff6bfe4d93c20c6414b8bc1b6392812
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-09-15 10:28:43 +0300

    create portfolio hover effect

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -25,8 +25,69 @@ $(document).ready(function () {
         $(".producth").slideToggle();
       })
 
+//portfolio hover behaviour
+      $("#p1").mouseenter(function(){
+        $("#w1").show();
+      })
+      .mouseleave(function(){
+        $("#w1").hide();
+      })
+
+      $("#p2").mouseenter(function(){
+        $("#w2").show();
+      })
+      .mouseleave(function(){
+        $("#w2").hide();
+      })
+
+      $("#p3").mouseenter(function(){
+        $("#w3").show();
+      })
+      .mouseleave(function(){
+        $("#w3").hide();
+      })
+
+      $("#p4").mouseenter(function(){
+        $("#w4").show();
+      })
+      .mouseleave(function(){
+        $("#w4").hide();
+      })
+
+      $("#p5").mouseenter(function(){
+        $("#w5").show();
+      })
+      .mouseleave(function(){
+        $("#w5").hide();
+      })
+
+      $("#p6").mouseenter(function(){
+        $("#w6").show();
+      })
+      .mouseleave(function(){
+        $("#w6").hide();
+      })
+
+      $("#p7").mouseenter(function(){
+        $("#w7").show();
+      })
+      .mouseleave(function(){
+        $("#w7").hide();
+      })
+
+      $("#p8").mouseenter(function(){
+        $("#w8").show();
+      })
+      .mouseleave(function(){
+        $("#w8").hide();
+      })
+
   }
 )
+
+
+
+
 
 //contact us processing
 
@@ -43,7 +104,7 @@ async function handleSubmit(event) {
       'Accept': 'application/json'
     }
   }).then(response => {
-    status.innerHTML = (document.getElementById("name").value) + " we have received your message. Thank you fro reaching out!";
+    status.innerHTML = (document.getElementById("name").value) + " we have received your message. Thank you for reaching out!";
     alert((document.getElementById("name").value) + " we have received your message. Thank you for reaching out!");
   
     form.reset()
